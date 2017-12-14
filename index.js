03612b79e1843041d852e01f1c2427dec6ae2334
Jon Osterman <suni.abraham@gmail.com>
2017-12-14 14:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveLastKitten(){
   return kittens
 }
 
-function destructivelyRemoveFirstKitten(kittens){
+function destructivelyRemoveFirstKitten(){
   kittens.shift();
   return kittens
 }
